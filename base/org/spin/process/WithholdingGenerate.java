6e8b5cd1dead8c1408e2d877bdebdab5e038dd95
Carlos Parada <carlosaparadam@gmail.com>
2019-09-30 16:18:07 -0400

    Fix Errors on Generate Withholding

--- base/org/spin/process/WithholdingGenerate.java
+++ base/org/spin/process/WithholdingGenerate.java
@@ -18,6 +18,7 @@
 package org.spin.process;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.adempiere.exceptions.AdempiereException;
@@ -109,86 +110,105 @@ public class WithholdingGenerate extends WithholdingGenerateAbstract {
 		
 		if (withholding.getC_Invoice_ID() > 0) {
 			MInvoice whDoc = (MInvoice) withholding.getC_Invoice();
-			if (whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_Completed)
-					|| whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_Closed)
-						|| whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_InProgress)
-							|| whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_Invalid)) {
+			if (whDoc!=null &&
+					(whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_Completed)
+						|| whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_Closed)
+							|| whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_InProgress)
+								|| whDoc.getDocStatus().equals(MInvoice.DOCSTATUS_Invalid))) {
 				addLog("@DocumentNo@ : " + whDoc.getDocumentNo() + " | @IsGenerated@ | @DocStatus@ : " + whDoc.getDocStatusName());
 				return;
 			}
 		}
-		MInvoice invoiceTo = null;
-		MInvoiceLine invoiceLineTo = null;
+		AtomicReference<Optional<MInvoice>> invoiceTo = new AtomicReference<>();
+		AtomicReference<Optional<MInvoiceLine>> invoiceLineTo = new AtomicReference<>();
 		
 		
 		AtomicReference<Integer> Curr_WH_Definition_ID = new AtomicReference<Integer>();
 		AtomicReference<Integer> Curr_WH_Setting_ID = new AtomicReference<Integer>();
 		AtomicReference<Integer> Curr_C_BPartner_ID = new AtomicReference<Integer>();
-		
+		AtomicReference<Integer> Curr_C_DocType_ID = new AtomicReference<Integer>();
 		
 		if (withholding.get_ID() > 0 ) {
 			Curr_WH_Definition_ID.set(withholding.getWH_Definition_ID());
 			Curr_WH_Setting_ID.set(withholding.getWH_Setting_ID());
 			Curr_C_BPartner_ID.set(withholding.getC_BPartner_ID());
+			Curr_C_DocType_ID.set(withholding.getWHDocType());
+			invoiceTo.set(Optional.empty());
+			invoiceLineTo.set(Optional.empty());
 			MWHDefinition whDefinition = (MWHDefinition)withholding.getWH_Definition();
 			MWHSetting whSetting = (MWHSetting)withholding.getWH_Setting();
 			MInvoice invoiceFrom = (MInvoice) withholding.getSourceInvoice();
-			Withholding withholldingDoc  = null;
+			Optional<Withholding> withholldingDoc  = Optional.empty();
 			if (!withholding.isManual())
 				withholldingDoc = withholdingDocList.stream()
 									.filter(wh ->(wh.getC_BPartner_ID()==Curr_C_BPartner_ID.get()
 													&& wh.getWH_Definition_ID()==Curr_WH_Definition_ID.get() 
-														&& wh.getWH_Setting_ID() == Curr_WH_Setting_ID.get()))
-									.findFirst()
-									.orElse(null);
+														&& wh.getWH_Setting_ID() == Curr_WH_Setting_ID.get())
+															&& wh.getC_DocType_ID() == Curr_C_DocType_ID.get())
+									.findFirst();
 
-			if (withholldingDoc==null) {
-				withholldingDoc = new Withholding(withholding.getWH_Definition_ID(), withholding.getWH_Setting_ID(), withholding.getC_BPartner_ID(), this);
-				invoiceTo = new MInvoice(getCtx(), 0, get_TrxName());
-				if (withholding.isManual())
-					invoiceTo.setDocumentNo(withholding.getDocumentNo());
+			if (!withholldingDoc.isPresent()) 
+				withholldingDoc = Optional.ofNullable(new Withholding(withholding.getWH_Definition_ID(), withholding.getWH_Setting_ID(), withholding.getC_BPartner_ID(), withholding.getWHDocType(), this));
+			
+			withholldingDoc.ifPresent(whDocument->{
+				if (!whDocument.getInvoice().isPresent()) {
+					MInvoice invoice = new MInvoice(getCtx(), 0, get_TrxName());
+					invoiceTo.set(Optional.ofNullable(invoice));
+					whDocument.setInvoice(invoice);
+				}else
+					invoiceTo.set(whDocument.getInvoice());
 				
-				invoiceTo.setAD_Org_ID(invoiceFrom.getAD_Org_ID());
-				invoiceTo.setC_BPartner_ID(invoiceFrom.getC_BPartner_ID());
-				invoiceTo.setC_BPartner_Location_ID(invoiceFrom.getC_BPartner_Location_ID());
-				invoiceTo.setM_PriceList_ID(invoiceFrom.getM_PriceList_ID());
-				invoiceTo.setIsSOTrx(invoiceFrom.isSOTrx());
-				invoiceTo.setDateInvoiced(getParameterAsTimestamp("DateDoc"));
-				invoiceTo.setDateAcct(getParameterAsTimestamp("DateDoc"));
-				int C_DocType_ID =  withholding.getWHDocType();
-				if (C_DocType_ID > 0)
-					invoiceTo.setC_DocTypeTarget_ID(C_DocType_ID);
-				else
-					throw new AdempiereException("@NotFound@ @WithholdingDebitDocType_ID@");
+				invoiceTo.get().ifPresent(invoice ->{
+					if (invoice.get_ID()==0) {
+						if (withholding.isManual())
+							invoice.setDocumentNo(withholding.getDocumentNo());
+						
+						invoice.setAD_Org_ID(invoiceFrom.getAD_Org_ID());
+						invoice.setC_BPartner_ID(invoiceFrom.getC_BPartner_ID());
+						invoice.setC_BPartner_Location_ID(invoiceFrom.getC_BPartner_Location_ID());
+						invoice.setM_PriceList_ID(invoiceFrom.getM_PriceList_ID());
+						invoice.setIsSOTrx(invoiceFrom.isSOTrx());
+						invoice.setDateInvoiced(getParameterAsTimestamp("DateDoc"));
+						invoice.setDateAcct(getParameterAsTimestamp("DateDoc"));
+						int C_DocType_ID =  Curr_C_DocType_ID.get();
+						if (C_DocType_ID > 0)
+							invoice.setC_DocTypeTarget_ID(C_DocType_ID);
+						else
+							throw new AdempiereException("@NotFound@ @WithholdingDebitDocType_ID@");
+						
+						invoice.setC_DocType_ID(invoice.getC_DocTypeTarget_ID());
+						invoice.saveEx();
+					}
+					
+					invoiceLineTo.set(Optional.ofNullable(new MInvoiceLine(invoice)));
+					invoiceLineTo.get().ifPresent(invoiceLine ->{
+						if (whSetting.getC_Charge_ID()> 0)
+							invoiceLine.setC_Charge_ID(whSetting.getC_Charge_ID());
+						else if (whDefinition.getC_Charge_ID()> 0)
+							invoiceLine.setC_Charge_ID(whDefinition.getC_Charge_ID());
+						else 
+							new AdempiereException("@NotFound@ @C_Charge_ID@");
+						
+						invoiceLine.setQty(Env.ONE);
+						invoiceLine.setPrice(withholding.getWithholdingAmt());
+						invoiceLine.saveEx();
+						
+						withholding.setC_Invoice_ID(invoiceLine.getC_Invoice_ID());
+						withholding.setC_InvoiceLine_ID(invoiceLine.getC_InvoiceLine_ID());
+						withholding.saveEx();
+						
+					});
+					
+					if (whDocument.getInvoice()==null)
+						whDocument.setInvoice(invoice);
+					
+					whDocument.addWithHolding(withholding);
+					
+				});
 				
-				invoiceTo.setC_DocType_ID(invoiceTo.getC_DocTypeTarget_ID());
-				invoiceTo.saveEx();
-			}
+				withholdingDocList.add(whDocument);
+			});
 			
-			if (invoiceTo==null)
-				invoiceTo = withholldingDoc.getInvoice();
-			
-			invoiceLineTo = new MInvoiceLine(invoiceTo);
-			if (whSetting.getC_Charge_ID()> 0)
-				invoiceLineTo.setC_Charge_ID(whSetting.getC_Charge_ID());
-			else if (whDefinition.getC_Charge_ID()> 0)
-				invoiceLineTo.setC_Charge_ID(whDefinition.getC_Charge_ID());
-			else 
-				new AdempiereException("@NotFound@ @C_Charge_ID@");
-			
-			invoiceLineTo.setQty(Env.ONE);
-			invoiceLineTo.setPrice(withholding.getWithholdingAmt());
-			invoiceLineTo.saveEx();
-			
-			withholding.setC_Invoice_ID(invoiceLineTo.getC_Invoice_ID());
-			withholding.setC_InvoiceLine_ID(invoiceLineTo.getC_InvoiceLine_ID());
-			withholding.saveEx();
-			
-			if (withholldingDoc.getInvoice()==null)
-				withholldingDoc.setInvoice(invoiceTo);
-			
-			withholldingDoc.addWithHolding(withholding);
-			withholdingDocList.add(withholldingDoc);
 		}
 	}
 	/**
@@ -210,6 +230,7 @@ class Withholding{
 	private int WH_Definition_ID = 0;
 	private int WH_Setting_ID = 0;
 	private int C_BPartner_ID = 0;
+	private int C_DocType_ID = 0;
 	private MInvoice invoice = null;
 	private MInvoiceLine invoiceLine = null;
 	private ArrayList<MWHWithholding> withholding = new ArrayList<MWHWithholding>();
@@ -222,10 +243,11 @@ class Withholding{
 	 * @param C_BPartner_ID
 	 * @param process
 	 */
-	public Withholding(int WH_Definition_ID, int WH_Setting_ID, int C_BPartner_ID, SvrProcess process) {
+	public Withholding(int WH_Definition_ID, int WH_Setting_ID, int C_BPartner_ID, int C_DocType_ID, SvrProcess process) {
 		this.WH_Definition_ID = WH_Definition_ID;
 		this.WH_Setting_ID = WH_Setting_ID;
 		this.C_BPartner_ID = C_BPartner_ID;
+		this.C_DocType_ID= C_DocType_ID; 
 		this.process = process;
 	}
 	
@@ -257,8 +279,8 @@ class Withholding{
 	 * Get Invoice
 	 * @return
 	 */
-	public MInvoice getInvoice() {
-		return invoice;
+	public Optional<MInvoice> getInvoice() {
+		return Optional.ofNullable(invoice);
 	}
 	
 	/**
@@ -312,6 +334,14 @@ class Withholding{
 			if (process!=null)
 				process.addLog("@DocumentNo@ : " + invoice.getDocumentNo());
 		}
+	}
+	
+	public int getC_DocType_ID() {
+		return C_DocType_ID;
+	}
+	
+	public void setC_DocType_ID(int c_DocType_ID) {
+		C_DocType_ID = c_DocType_ID;
 	}
 	
 }
